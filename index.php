5a09745698efd5dc4662d779650176ccf590a53c
JackNel <jcneligan@gmail.com>
2018-05-29 20:05:39 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+?>
+
+<h1>Get in touch</h1>
+
+<form method="post">
+    <p>Email address</p>
+    <input name="email" placeholder="Enter email" type="text"/>
+    <p>Subject</p>
+    <input name="subject" type="text"/>
+    <p>What would you like to ask us?</p>
+    <input type="submit" value="Submit"/>
+</form>
