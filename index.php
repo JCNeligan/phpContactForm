c4a489b25479bbe99c0ce4df87263ed6007e85ae
JackNel <jcneligan@gmail.com>
2018-05-30 13:25:27 -0400

    Check if subject field and content field are blank

--- index.php
+++ index.php
@@ -14,25 +14,46 @@
     <div class="container">
         <h1>Get in touch!</h1>
 
-        <form method="post">
+        <div id="error">
+
+        </div>
+
+        <form>
             <div class="form-group">
                 <label for="email">Email address</label>
-                <input id="email" class="form-control" name="email" placeholder="Enter email" type="text"/>
-                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                <input id="email" class="form-control" placeholder="Enter your email address" type="email"/>
+                <small class="form-text text-muted">We'll never share your email with anyone else.</small>
             </div>
             <div class="form-group">
                 <label for="subject">Subject</label>
-                <input id="subject" class="form-control" name="subject" type="text"/>
+                <input id="subject" class="form-control" type="text"/>
             </div>
             <div class="form-group">
-                <label for="questionField">What would you like to ask us?</label>
-                <textarea id="questionField" class="form-control" rows="3"></textarea>
+                <label for="contentField">What would you like to ask us?</label>
+                <textarea id="contentField" class="form-control" rows="3"></textarea>
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" id="submit" class="btn btn-primary">Submit</button>
+
         </form>
     </div>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+
+    <script>
+        $("form").submit(function (e) {
+            e.preventDefault();
+            var error = "";
+
+            // Check if subject field is blank
+            if($("#subject").val() == "") {
+                error += "<p>The subject field is required.</p>"
+            }
+            if($("#contentField").val() == "") {
+                error += "<p>The content field is required.</p>"
+            }
+            $("#error").html(error);
+        });
+    </script>
   </body>
 </html>
