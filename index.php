24041d6e7ea981a85714806da4196169fae23029
JackNel <jcneligan@gmail.com>
2018-05-29 23:01:59 -0400

    Update form to bootsrap form

--- index.php
+++ index.php
@@ -11,17 +11,26 @@
     <title>Contact Form</title>
   </head>
   <body>
-<h1>Get in touch</h1>
+    <div class="container">
+        <h1>Get in touch!</h1>
 
-<form method="post">
-    <p>Email address</p>
-    <input name="email" placeholder="Enter email" type="text"/>
-    <p>Subject</p>
-    <input name="subject" type="text"/>
-    <p>What would you like to ask us?</p>
-    <input type="submit" value="Submit"/>
-</form>
-
+        <form method="post">
+            <div class="form-group">
+                <label for="email">Email address</label>
+                <input id="email" class="form-control" name="email" placeholder="Enter email" type="text"/>
+                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+            <div class="form-group">
+                <label for="subject">Subject</label>
+                <input id="subject" class="form-control" name="subject" type="text"/>
+            </div>
+            <div class="form-group">
+                <label for="questionField">What would you like to ask us?</label>
+                <textarea id="questionField" class="form-control" rows="3"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
